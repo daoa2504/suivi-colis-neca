9c1621a94f8834976eaf867a5ec4ed368c113d52
daoa2504 <daoa2504@usherbrooke.ca>
2025-09-04 18:17:00 -0400

    Correction bug hébergement

--- src/app/api/shipments/[trackingId]/events/route.ts
+++ src/app/api/shipments/[trackingId]/events/route.ts
@@ -1,9 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
-import type { Prisma } from "@prisma/client";
 import { addEventSchema } from "@/lib/validators";
 import { resend, FROM, BASE_URL } from "@/lib/email";
-import { ShipmentStatus } from "@prisma/client";
-import {prisma} from "@/lib/prisma";
+import type { Prisma } from "@prisma/client";   // ✅ import de type
+
+
+type ShipmentStatus =
+    | "CREATED"
+    | "RECEIVED_IN_GUINEA"
+    | "IN_TRANSIT"
+    | "IN_CUSTOMS"
+    | "ARRIVED_IN_CANADA"
+    | "PICKED_UP"
+    | "OUT_FOR_DELIVERY"
+    | "DELIVERED";
+ // ✅ type de l'enum
 
 type EventType =
     | "RECEIVED_IN_GUINEA"
